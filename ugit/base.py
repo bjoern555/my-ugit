4bbd302935f638b2195f380ab9eaab719dc7a18c
bjoern555 <bjoern-wagner@outlook.com>
2025-12-07 12:54:29 +0800

    Add command ugit branch

--- ugit/base.py
+++ ugit/base.py
@@ -3,7 +3,7 @@ import operator
 import os
 import string
 
-from collections import namedtuple
+from collections import deque, namedtuple
 
 from . import data
 
@@ -110,6 +110,10 @@ def create_tag(name, oid):
     data.update_ref(f"refs/tags/{name}", oid)
 
 
+def create_branch(name, oid):
+    data.update_ref(f"refs/heads/{name}", oid)
+
+
 Commit = namedtuple("Commit", ["tree", "parent", "message"])
 
 
@@ -132,18 +136,18 @@ def get_commit(oid):
 
 
 def iter_commits_and_parents(oids):
-    oids = set(oids)
+    oids = deque(oids)
     visited = set()
 
     while oids:
-        oid = oids.pop()
+        oid = oids.popleft()
         if not oid or oid in visited:
             continue
         visited.add(oid)
         yield oid
 
         commit = get_commit(oid)
-        oids.add(commit.parent)
+        oids.appendleft(commit.parent)
 
 
 def get_oid(name):
